d7ecc1cf26f0b164494ba8520e998e5310194cd4
Ryuya Ishibashi <ryuya-ishibashi@daiwa-sc.co.jp>
2020-12-02 17:37:22 +0900

    最初のコミット

---
+++ tests/unit/Bilingual.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Bilingual from '@/components/Bilingual.vue'
+
+describe('Bilingual', () => {
+    it('reders successfully', () => {
+        const wrapper = mount(Bilingual)
+
+        console.log(wrapper.html())
+    })
+})
---
+++ tests/unit/Emitter.spec.js
@@ -0,0 +1,21 @@
+import Emitter from '@/components/Emitter.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe('Emitter', () => {
+    it('２つの引数があるイベントを発火する', () => {
+        const wrapper = shallowMount(Emitter)
+
+        wrapper.vm.emitEvent()
+
+        expect(wrapper.emitted().myEvent[0]).toEqual(['name', 'password'])
+    })
+
+    it('コンポーネントをレンダーせずにイベントを検証する', () => {
+        const events = {}
+        const $emit = (event, ...args) => { events[event] = [...args] }
+
+        Emitter.methods.emitEvent.call({ $emit })
+        
+        expect(events.myEvent).toEqual(['name', 'password'])
+    })
+})
